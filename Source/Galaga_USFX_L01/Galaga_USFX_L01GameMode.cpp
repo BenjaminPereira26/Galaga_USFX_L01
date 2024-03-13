ffd95fa789f9fa2803c027e3571ff3f53866bb23
BenjaminPereira26 <160974557+BenjaminPereira26@users.noreply.github.com>
2024-03-13 00:46:06 -0400

    dos filas de clases nietas spawneadas en el world

--- Source/Galaga_USFX_L01/Galaga_USFX_L01GameMode.cpp
+++ Source/Galaga_USFX_L01/Galaga_USFX_L01GameMode.cpp
@@ -7,6 +7,17 @@
 #include "NaveEnemigaCaza.h"
 #include "NaveEnemigaEspia.h"
 #include "NaveEnemigaCazaNieta01.h"
+#include "NaveEnemigaCazaNieta02.h"
+#include "NaveEnemigaEspiaNieta01.h"
+#include "NaveEnemigaEspiaNieta02.h"
+#include "NaveEnemigaKamikazeNieta01.h"
+#include "NaveEnemigaKamikazeNieta02.h"
+#include "NaveEnemigaNodrizaNieta01.h"
+#include "NaveEnemigaNodrizaNieta02.h"
+#include "NaveEnemigaReabastecimientoN01.h"
+#include "NaveEnemigaReabastecimientoN02.h"
+#include "NaveEnemigaTransporteNieta01.h"
+#include "NaveEnemigaTransporteNieta02.h"
 
 AGalaga_USFX_L01GameMode::AGalaga_USFX_L01GameMode()
 {
@@ -23,7 +34,36 @@ void AGalaga_USFX_L01GameMode::BeginPlay()
 
 	FVector ubicacionNave01 = FVector(-1000.0f, 500.0f, 250.0f);
 	FVector ubicacionNave02 = FVector(-500.0f, -500.0f, 250.0f);
-	FVector ubicacionNaveEnemigaCazaNieta01_1 = FVector(700.0f, 500.0f, 250.0f);
+
+	FVector ubicacionNaveEnemigaCazaNieta01_1 = FVector(703.0f, 604.0f, 250.0f);
+	FVector ubicacionNaveEnemigaCazaNieta01_2 = FVector(807.0f, 610.0f, 250.0f);
+	FVector ubicacionNaveEnemigaCazaNieta02_1 = FVector(800.0f, 500.0f, 250.0f);
+	FVector ubicacionNaveEnemigaCazaNieta02_2 = FVector(700.0f, 500.0f, 250.0f);
+
+	FVector ubicacionNaveEnemigaEspiaNieta01_1 = FVector(802.0f, 385.0f, 250.0f);
+	FVector ubicacionNaveEnemigaEspiaNieta01_2 = FVector(800.0f, 262.0f, 250.0f);
+	FVector ubicacionNaveEnemigaEspiaNieta02_1 = FVector(695.0f, 252.0f, 250.0f);
+	FVector ubicacionNaveEnemigaEspiaNieta02_2 = FVector(694.0f, 386.0f, 250.0f);
+
+	FVector ubicacionNaveEnemigaKamikazeNieta01_1 = FVector(797.0f, 134.0f, 250.0f);
+	FVector ubicacionNaveEnemigaKamikazeNieta01_2 = FVector(796.0f, -10.0f, 250.0f);
+	FVector ubicacionNaveEnemigaKamikazeNieta02_1 = FVector(687.0f, 143.0f, 250.0f);
+	FVector ubicacionNaveEnemigaKamikazeNieta02_2 = FVector(692.0f,  -5.0f, 250.0f);
+
+	FVector ubicacionNaveEnemigaNodrizaNieta01_1 = FVector(797.0f, -125.0f, 250.0f);
+	FVector ubicacionNaveEnemigaNodrizaNieta01_2 = FVector(796.0f, -235.0f, 250.0f);
+	FVector ubicacionNaveEnemigaNodrizaNieta02_1 = FVector(687.0f, -118.0f, 250.0f);
+	FVector ubicacionNaveEnemigaNodrizaNieta02_2 = FVector(692.0f, -235.0f, 250.0f);
+
+	FVector ubicacionNaveEnemigaReabastecimientoN01_1 = FVector(797.0f, -469.0f, 250.0f);
+	FVector ubicacionNaveEnemigaReabastecimientoN01_2 = FVector(796.0f, -342.0f, 250.0f);
+	FVector ubicacionNaveEnemigaReabastecimientoN02_1 = FVector(687.0f, -469.0f, 250.0f);
+	FVector ubicacionNaveEnemigaReabastecimientoN02_2 = FVector(692.0f, -342.0f, 250.0f);
+
+	FVector ubicacionNaveEnemigaTransporteNieta01_1 = FVector(797.0f, -668.0f, 250.0f);
+	FVector ubicacionNaveEnemigaTransporteNieta01_2 = FVector(796.0f, -563.0f, 250.0f);
+	FVector ubicacionNaveEnemigaTransporteNieta02_1 = FVector(687.0f, -668.0f, 250.0f);
+	FVector ubicacionNaveEnemigaTransporteNieta02_2 = FVector(692.0f, -563.0f, 250.0f);
 
 	FRotator rotacionNave = FRotator(0.0f, 0.0f, 0.0f);
 
@@ -33,11 +73,68 @@ void AGalaga_USFX_L01GameMode::BeginPlay()
 		// spawn the projectile
 		NaveEnemigaTransporte01 = World->SpawnActor<ANaveEnemigaTransporte>(ubicacionNave01, rotacionNave);
 		NaveEnemigaCaza01 = World->SpawnActor<ANaveEnemigaCaza>(ubicacionNave02, rotacionNave);
-		NaveEnemigaCazaNieta01_1 = World->SpawnActor<ANaveEnemigaCazaNieta01>(ubicacionNaveEnemigaCazaNieta01_1, rotacionNave);
 
+		NaveEnemigaCazaNieta01_1 = World->SpawnActor<ANaveEnemigaCazaNieta01>(ubicacionNaveEnemigaCazaNieta01_1, rotacionNave);
+		NaveEnemigaCazaNieta01_2 = World->SpawnActor<ANaveEnemigaCazaNieta01>(ubicacionNaveEnemigaCazaNieta01_2, rotacionNave);
+		NaveEnemigaCazaNieta02_1 = World->SpawnActor<ANaveEnemigaCazaNieta02>(ubicacionNaveEnemigaCazaNieta02_1, rotacionNave);
+		NaveEnemigaCazaNieta02_2 = World->SpawnActor<ANaveEnemigaCazaNieta02>(ubicacionNaveEnemigaCazaNieta02_2, rotacionNave);
+
+		NaveEnemigaEspiaNieta01_1 = World->SpawnActor<ANaveEnemigaEspiaNieta01>(ubicacionNaveEnemigaEspiaNieta01_1, rotacionNave);
+		NaveEnemigaEspiaNieta01_2 = World->SpawnActor<ANaveEnemigaEspiaNieta01>(ubicacionNaveEnemigaEspiaNieta01_2, rotacionNave);
+		NaveEnemigaEspiaNieta02_1 = World->SpawnActor<ANaveEnemigaEspiaNieta02>(ubicacionNaveEnemigaEspiaNieta02_1, rotacionNave);
+		NaveEnemigaEspiaNieta02_2 = World->SpawnActor<ANaveEnemigaEspiaNieta02>(ubicacionNaveEnemigaEspiaNieta02_2, rotacionNave);
+
+		NaveEnemigaKamikazeNieta01_1 = World->SpawnActor<ANaveEnemigaKamikazeNieta01>(ubicacionNaveEnemigaKamikazeNieta01_1, rotacionNave);
+		NaveEnemigaKamikazeNieta01_2 = World->SpawnActor<ANaveEnemigaKamikazeNieta01>(ubicacionNaveEnemigaKamikazeNieta01_2, rotacionNave);
+		NaveEnemigaKamikazeNieta02_1 = World->SpawnActor<ANaveEnemigaKamikazeNieta02>(ubicacionNaveEnemigaKamikazeNieta02_1, rotacionNave);
+		NaveEnemigaKamikazeNieta02_2 = World->SpawnActor<ANaveEnemigaKamikazeNieta02>(ubicacionNaveEnemigaKamikazeNieta02_2, rotacionNave);
+
+		NaveEnemigaNodrizaNieta01_1 = World->SpawnActor<ANaveEnemigaNodrizaNieta01>(ubicacionNaveEnemigaNodrizaNieta01_1, rotacionNave);
+		NaveEnemigaNodrizaNieta01_2 = World->SpawnActor<ANaveEnemigaNodrizaNieta01>(ubicacionNaveEnemigaNodrizaNieta01_2, rotacionNave);
+		NaveEnemigaNodrizaNieta02_1 = World->SpawnActor<ANaveEnemigaNodrizaNieta02>(ubicacionNaveEnemigaNodrizaNieta02_1, rotacionNave);
+		NaveEnemigaNodrizaNieta02_2 = World->SpawnActor<ANaveEnemigaNodrizaNieta02>(ubicacionNaveEnemigaNodrizaNieta02_2, rotacionNave);
+
+		NaveEnemigaReabastecimientoN01_1 = World->SpawnActor<ANaveEnemigaReabastecimientoN01>(ubicacionNaveEnemigaReabastecimientoN01_1, rotacionNave);
+		NaveEnemigaReabastecimientoN01_2 = World->SpawnActor<ANaveEnemigaReabastecimientoN01>(ubicacionNaveEnemigaReabastecimientoN01_2, rotacionNave);
+		NaveEnemigaReabastecimientoN02_1 = World->SpawnActor<ANaveEnemigaReabastecimientoN02>(ubicacionNaveEnemigaReabastecimientoN02_1, rotacionNave);
+		NaveEnemigaReabastecimientoN02_2 = World->SpawnActor<ANaveEnemigaReabastecimientoN02>(ubicacionNaveEnemigaReabastecimientoN02_2, rotacionNave);
+
+		NaveEnemigaTransporteNieta01_1 = World->SpawnActor<ANaveEnemigaTransporteNieta01>(ubicacionNaveEnemigaTransporteNieta01_1, rotacionNave);
+		NaveEnemigaTransporteNieta01_2 = World->SpawnActor<ANaveEnemigaTransporteNieta01>(ubicacionNaveEnemigaTransporteNieta01_2, rotacionNave);
+		NaveEnemigaTransporteNieta02_1 = World->SpawnActor<ANaveEnemigaTransporteNieta02>(ubicacionNaveEnemigaTransporteNieta02_1, rotacionNave);
+		NaveEnemigaTransporteNieta02_2 = World->SpawnActor<ANaveEnemigaTransporteNieta02>(ubicacionNaveEnemigaTransporteNieta02_2, rotacionNave);
 	}
 
 	NaveEnemigaCaza01->SetPosicion(FVector(-500.0f, 500.0f, 200.0f));
-	NaveEnemigaTransporte01->SetPosicion(FVector(500.0f, -500.0f, 200.0f));
-	NaveEnemigaCazaNieta01_1->SetPosicion(FVector(800.0f, -500.0f, 200.0f));
+	NaveEnemigaTransporte01->SetPosicion(FVector(-1000.0f, 500.0f, 250.0f));
+
+	NaveEnemigaCazaNieta01_1->SetPosicion(FVector(703.0f, 604.0f, 250.0f));
+	NaveEnemigaCazaNieta01_2->SetPosicion(FVector(807.0f, 610.0f, 250.0f));
+	NaveEnemigaCazaNieta02_1->SetPosicion(FVector(800.0f, 500.0f, 250.0f));
+	NaveEnemigaCazaNieta02_2->SetPosicion(FVector(700.0f, 500.0f, 250.0f));
+
+	NaveEnemigaEspiaNieta01_1->SetPosicion(FVector(802.0f, 385.0f, 250.0f));
+	NaveEnemigaEspiaNieta01_2->SetPosicion(FVector(800.0f, 262.0f, 250.0f));
+	NaveEnemigaEspiaNieta02_1->SetPosicion(FVector(695.0f, 252.0f, 250.0f));
+	NaveEnemigaEspiaNieta02_2->SetPosicion(FVector(694.0f, 386.0f, 250.0f));
+
+	NaveEnemigaKamikazeNieta01_1->SetPosicion(FVector(797.0f, 134.0f, 250.0f));
+	NaveEnemigaKamikazeNieta01_2->SetPosicion(FVector(796.0f, -10.0f, 250.0f));
+	NaveEnemigaKamikazeNieta02_1->SetPosicion(FVector(687.0f, 143.0f, 250.0f));
+	NaveEnemigaKamikazeNieta02_2->SetPosicion(FVector(692.0f,  -5.0f, 250.0f));
+
+	NaveEnemigaNodrizaNieta01_1->SetPosicion(FVector(797.0f, -125.0f, 250.0f));
+	NaveEnemigaNodrizaNieta01_2->SetPosicion(FVector(796.0f, -235.0f, 250.0f));
+	NaveEnemigaNodrizaNieta02_1->SetPosicion(FVector(687.0f, -118.0f, 250.0f));
+	NaveEnemigaNodrizaNieta02_2->SetPosicion(FVector(692.0f, -235.0f, 250.0f));
+
+	NaveEnemigaReabastecimientoN01_1->SetPosicion(FVector(797.0f, -469.0f, 250.0f));
+	NaveEnemigaReabastecimientoN01_2->SetPosicion(FVector(796.0f, -342.0f, 250.0f));
+	NaveEnemigaReabastecimientoN02_1->SetPosicion(FVector(687.0f, -469.0f, 250.0f));
+	NaveEnemigaReabastecimientoN02_2->SetPosicion(FVector(692.0f, -342.0f, 250.0f));
+
+	NaveEnemigaTransporteNieta01_1->SetPosicion(FVector(797.0f, -668.0f, 250.0f));
+	NaveEnemigaTransporteNieta01_2->SetPosicion(FVector(796.0f, -563.0f, 250.0f));
+	NaveEnemigaTransporteNieta02_1->SetPosicion(FVector(687.0f, -668.0f, 250.0f));
+	NaveEnemigaTransporteNieta02_2->SetPosicion(FVector(692.0f, -563.0f, 250.0f));
 }
--- Source/Galaga_USFX_L01/Galaga_USFX_L01GameMode.h
+++ Source/Galaga_USFX_L01/Galaga_USFX_L01GameMode.h
@@ -9,7 +9,24 @@
 //class ANaveEnemiga;
 class ANaveEnemigaTransporte;
 class ANaveEnemigaCaza;
+
 class ANaveEnemigaCazaNieta01;
+class ANaveEnemigaCazaNieta02;
+
+class ANaveEnemigaEspiaNieta01;
+class ANaveEnemigaEspiaNieta02;
+
+class ANaveEnemigaKamikazeNieta01;
+class ANaveEnemigaKamikazeNieta02;
+
+class ANaveEnemigaNodrizaNieta01;
+class ANaveEnemigaNodrizaNieta02;
+
+class ANaveEnemigaReabastecimientoN01;
+class ANaveEnemigaReabastecimientoN02;
+
+class ANaveEnemigaTransporteNieta01;
+class ANaveEnemigaTransporteNieta02;
 
 UCLASS(MinimalAPI)
 class AGalaga_USFX_L01GameMode : public AGameModeBase
@@ -23,7 +40,37 @@ public:
 	//ANaveEnemiga* NaveEnemiga01;
 	ANaveEnemigaTransporte* NaveEnemigaTransporte01;
 	ANaveEnemigaCaza* NaveEnemigaCaza01;
+
 	ANaveEnemigaCazaNieta01* NaveEnemigaCazaNieta01_1;
+	ANaveEnemigaCazaNieta01* NaveEnemigaCazaNieta01_2;
+	ANaveEnemigaCazaNieta02* NaveEnemigaCazaNieta02_1;
+	ANaveEnemigaCazaNieta02* NaveEnemigaCazaNieta02_2;
+
+	ANaveEnemigaEspiaNieta01* NaveEnemigaEspiaNieta01_1;
+	ANaveEnemigaEspiaNieta01* NaveEnemigaEspiaNieta01_2;
+	ANaveEnemigaEspiaNieta02* NaveEnemigaEspiaNieta02_1;
+	ANaveEnemigaEspiaNieta02* NaveEnemigaEspiaNieta02_2;
+
+	ANaveEnemigaKamikazeNieta01* NaveEnemigaKamikazeNieta01_1;
+	ANaveEnemigaKamikazeNieta01* NaveEnemigaKamikazeNieta01_2;
+	ANaveEnemigaKamikazeNieta02* NaveEnemigaKamikazeNieta02_1;
+	ANaveEnemigaKamikazeNieta02* NaveEnemigaKamikazeNieta02_2;
+
+	ANaveEnemigaNodrizaNieta01* NaveEnemigaNodrizaNieta01_1;
+	ANaveEnemigaNodrizaNieta01* NaveEnemigaNodrizaNieta01_2;
+	ANaveEnemigaNodrizaNieta02* NaveEnemigaNodrizaNieta02_1;
+	ANaveEnemigaNodrizaNieta02* NaveEnemigaNodrizaNieta02_2;
+
+	ANaveEnemigaReabastecimientoN01* NaveEnemigaReabastecimientoN01_1;
+	ANaveEnemigaReabastecimientoN01* NaveEnemigaReabastecimientoN01_2;
+	ANaveEnemigaReabastecimientoN02* NaveEnemigaReabastecimientoN02_1;
+	ANaveEnemigaReabastecimientoN02* NaveEnemigaReabastecimientoN02_2;
+
+	ANaveEnemigaTransporteNieta01* NaveEnemigaTransporteNieta01_1;
+	ANaveEnemigaTransporteNieta01* NaveEnemigaTransporteNieta01_2;
+	ANaveEnemigaTransporteNieta02* NaveEnemigaTransporteNieta02_1;
+	ANaveEnemigaTransporteNieta02* NaveEnemigaTransporteNieta02_2;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
