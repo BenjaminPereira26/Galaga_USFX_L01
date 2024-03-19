f0490a224efc57d1e3903355ee48d221087a8616
BenjaminPereira26 <160974557+BenjaminPereira26@users.noreply.github.com>
2024-03-19 16:00:32 -0400

    arrays de enemigos con tmap

--- Source/Galaga_USFX_L01/Galaga_USFX_L01GameMode.h
+++ Source/Galaga_USFX_L01/Galaga_USFX_L01GameMode.h
@@ -6,27 +6,10 @@
 #include "GameFramework/GameModeBase.h"
 #include "Galaga_USFX_L01GameMode.generated.h"
 
-//class ANaveEnemiga;
+class ANaveEnemiga;
 class ANaveEnemigaTransporte;
 class ANaveEnemigaCaza;
-
-class ANaveEnemigaCazaNieta01;
-class ANaveEnemigaCazaNieta02;
-
-class ANaveEnemigaEspiaNieta01;
-class ANaveEnemigaEspiaNieta02;
-
-class ANaveEnemigaKamikazeNieta01;
-class ANaveEnemigaKamikazeNieta02;
-
-class ANaveEnemigaNodrizaNieta01;
-class ANaveEnemigaNodrizaNieta02;
-
-class ANaveEnemigaReabastecimientoN01;
-class ANaveEnemigaReabastecimientoN02;
-
-class ANaveEnemigaTransporteNieta01;
-class ANaveEnemigaTransporteNieta02;
+class ANaveEnemigaKamikaze;
 
 UCLASS(MinimalAPI)
 class AGalaga_USFX_L01GameMode : public AGameModeBase
@@ -40,38 +23,26 @@ public:
 	//ANaveEnemiga* NaveEnemiga01;
 	ANaveEnemigaTransporte* NaveEnemigaTransporte01;
 	ANaveEnemigaCaza* NaveEnemigaCaza01;
-
-	ANaveEnemigaCazaNieta01* NaveEnemigaCazaNieta01_1;
-	ANaveEnemigaCazaNieta01* NaveEnemigaCazaNieta01_2;
-	ANaveEnemigaCazaNieta02* NaveEnemigaCazaNieta02_1;
-	ANaveEnemigaCazaNieta02* NaveEnemigaCazaNieta02_2;
-
-	ANaveEnemigaEspiaNieta01* NaveEnemigaEspiaNieta01_1;
-	ANaveEnemigaEspiaNieta01* NaveEnemigaEspiaNieta01_2;
-	ANaveEnemigaEspiaNieta02* NaveEnemigaEspiaNieta02_1;
-	ANaveEnemigaEspiaNieta02* NaveEnemigaEspiaNieta02_2;
-
-	ANaveEnemigaKamikazeNieta01* NaveEnemigaKamikazeNieta01_1;
-	ANaveEnemigaKamikazeNieta01* NaveEnemigaKamikazeNieta01_2;
-	ANaveEnemigaKamikazeNieta02* NaveEnemigaKamikazeNieta02_1;
-	ANaveEnemigaKamikazeNieta02* NaveEnemigaKamikazeNieta02_2;
-
-	ANaveEnemigaNodrizaNieta01* NaveEnemigaNodrizaNieta01_1;
-	ANaveEnemigaNodrizaNieta01* NaveEnemigaNodrizaNieta01_2;
-	ANaveEnemigaNodrizaNieta02* NaveEnemigaNodrizaNieta02_1;
-	ANaveEnemigaNodrizaNieta02* NaveEnemigaNodrizaNieta02_2;
-
-	ANaveEnemigaReabastecimientoN01* NaveEnemigaReabastecimientoN01_1;
-	ANaveEnemigaReabastecimientoN01* NaveEnemigaReabastecimientoN01_2;
-	ANaveEnemigaReabastecimientoN02* NaveEnemigaReabastecimientoN02_1;
-	ANaveEnemigaReabastecimientoN02* NaveEnemigaReabastecimientoN02_2;
-
-	ANaveEnemigaTransporteNieta01* NaveEnemigaTransporteNieta01_1;
-	ANaveEnemigaTransporteNieta01* NaveEnemigaTransporteNieta01_2;
-	ANaveEnemigaTransporteNieta02* NaveEnemigaTransporteNieta02_1;
-	ANaveEnemigaTransporteNieta02* NaveEnemigaTransporteNieta02_2;
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+public:
+	TArray<ANaveEnemiga*> TANavesEnemigas;
+	TArray<ANaveEnemigaCaza*> TANavesEnemigasCaza;
+	TArray<ANaveEnemigaTransporte*> TANavesEnemigasTransporte;
+	TArray<ANaveEnemigaKamikaze*> TANavesEnemigasKamikaze;
+
+private:
+	int TiempoTranscurrido;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	// Declaración del TMap
+	TMap<int32, TArray<ANaveEnemigaCaza*>> ColumnaNavesEnemigasCaza;
+	//Clave int 32, valor array de naves enemigas
+
 };
